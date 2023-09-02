33ecf5dd5b95e9b68c3eab45eefff126998c7072
Ian <ian.odhiambo@student.moringaschool.com>
2023-09-02 12:20:09 +0300

    Tables

--- migration.py
+++ migration.py
@@ -21,4 +21,17 @@ class Customer(Base):
 
     id = Column (Integer , primary_key=True)
     first_name = Column(String(255) , nullable=False)
-    
+    last_name = Column(String(255) , nullable=False)
+
+    reviews = relationship('Review', bankref='customer')
+
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column (Integer , primary_key = True)
+    star_rating = Column(Integer)
+
+
+    restaurant_id = Column(Integer , ForeignKey('restaurants.id') , nullable=False)
+    customer_id = Column(Integer , ForeignKey('customer.id') , nullable=False)
